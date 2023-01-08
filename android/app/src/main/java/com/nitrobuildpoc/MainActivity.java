77afaa3be4e64087b707aeea3216b2a2c4adc861
David <4661784+retyui@users.noreply.github.com>
2023-01-08 14:28:14 +0100

    Invalidate nitro cache

--- android/app/src/main/java/com/nitrobuildpoc/MainActivity.java
+++ android/app/src/main/java/com/nitrobuildpoc/MainActivity.java
@@ -5,6 +5,7 @@ import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
 
+// trigger ci
 public class MainActivity extends ReactActivity {
 
   /**
