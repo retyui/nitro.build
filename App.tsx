5dcec2ce50177e1c03f0c037fe304505a6e93901
David <4661784+retyui@users.noreply.github.com>
2023-01-28 13:27:19 +0100

    Maestro Cloud

--- App.tsx
+++ App.tsx
@@ -1,112 +1,50 @@
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-// js2
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import React, {useState} from 'react';
+import {Text, Button, SafeAreaView, TextInput, ViewStyle} from 'react-native';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+const initialState = {
+  email: '',
+  password: '',
+  access: false,
+};
 
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+const $root = {
+  justifyContent: 'center',
+  flex: 1,
+  paddingHorizontal: 16,
+} as ViewStyle;
 
-function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+function App() {
+  const [state, setState] = useState(initialState);
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const onSignIn = () => {
+    setState(p => ({
+      ...p,
+      access: p.email === 'test@test.com' && p.password === '123456',
+    }));
   };
 
+  if (!state.access) {
+    return (
+      <SafeAreaView style={$root}>
+        <Text>Login</Text>
+        <TextInput
+          placeholder="Enter your email"
+          onChangeText={email => setState(p => ({...p, email}))}
+        />
+        <TextInput
+          placeholder="Enter your password"
+          onChangeText={password => setState(p => ({...p, password}))}
+        />
+        <Button title="Sign in" onPress={onSignIn} />
+      </SafeAreaView>
+    );
+  }
+
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            JS change
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+    <SafeAreaView style={$root}>
+      <Text>Hi, {state.email}!</Text>
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
